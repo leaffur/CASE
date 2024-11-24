2da05eace90dbb114983bd595b28c4d19db2b02b
Chen Lin <cl2384@login2.mccleary.ycrc.yale.edu>
2024-11-24 01:33:22 -0500

    Create the CASE version 0.0.1.0

---
+++ R/CASE1.0.R
@@ -0,0 +1,434 @@
+# source("/gpfs/gibbs/pi/zhao/cl2384/scEQTL/ExtremeD/CASE_uitility.R")
+
+#' @title CASE Model Training
+#' 
+#' @description Fit the priors for the cis-eQTL effect sizes.
+#' @author Chen Lin, Hongyu Zhao
+#' @details TBD
+#' @references TBD
+#' @param Z M * C matrix of z scores.
+#' @param R M * M matrix of LD.
+#' @param hatB M * C matrix of the estimated effects. Alternative summary data (together with hatS) to be provided instead of Z.
+#' @param hatB M * C matrix of standard errors of the estimated effects. Alternative summary data (together with hatB) to be provided instead of Z.
+#' @return A \code{"CASE"} object with the following elements:
+#' \item{pi:}{L-vector, the prior probabilities of sharing patterns.}
+#' \item{U:}{L-list of C * C matrix, the prior covariances of sharing patterns.}
+#' \item{V:}{C * C matrix, the sample-adjusted phenotypical variance.}
+#' @examples 
+#' TBD
+#' @importFrom stats var
+#' @export
+ED_train <- function(Z = NULL, R, N, hatB = NULL, hatS = NULL, 
+                     V = NULL, h = NULL,
+                     pi.init = NULL, U.orig = NULL,  
+                     V.fix = TRUE, pi.fix = FALSE, 
+                     n.iter = 45, MC.max = 125, tol = 1e-2, MC.seq = "exp", 
+                     significant_thres = 0.1, noise_threshold = 0.1){
+  ## hatB: G list with matrix M_j * C as the eQTL summary stats for each gene.
+  ## R: Glist with M_j * M_j matrix: LD matrix for each region.
+  ## N: C * C matrix, sample size and overlapped structure for each cell type.
+  ##    Can be C vector which assumes full overlaps
+  ## h: G * C matrix of cis-heritability.
+  ## V: C * C covariance for the noise between cell types.
+  
+  
+  ## U.orig: t1 + t2 lists / array with matrices C * C, initial cell type correlations for EM algorithm.
+  ## pi.init: t1 + t2 + 1 vector, initial guess for prob of correlations.
+  ##          The last is prob of the delta mass of zero.
+  if (is.null(Z)){
+    Z = hatB / hatS
+  }
+  
+  hatBS = transform_Z(Z, N)
+  hatB = hatBS$hatB
+  hatS = hatBS$hatS
+  
+  
+  if (MC.seq == "fix"){
+    MC.sim = rep(MC.max, n.iter)
+  }else if (MC.seq == "linear"){
+    MC.sim = seq(25, MC.max, length.out = n.iter) %>% round
+  }else if (MC.seq == "exp"){
+    MC.sim = ((MC.max / 60)^((1:n.iter-1) / (n.iter - 1)) * 60) %>% round
+  }
+
+  C <- ncol(hatB)
+  
+  if (is.null(dim(N))){
+    N = diag(N)
+    for (i in 1:(C-1)){
+      for (j in (i+1):C){
+        N[j, i] = N[i, j] = min(N[i, i], N[j, j])
+      }
+    }
+  }
+  
+  if (is.null(V)){
+    V = diag(rep(1, C))
+    for (i in 1:C){
+      for (j in 1:C){
+        V[i, j] = V[i, j] * N[i, j] / (N[i, i] * N[j, j])
+      }
+    }
+  }
+  
+  # ME_p <- 2 - 2 * pnorm(abs(hatB / hatS), 0, 1)
+  # idx <- apply(ME_p > noise_threshold, 1, all)
+  # if (length(idx) <= 1e5){
+  #   V = array(0, dim = c(C, C, G))
+  # } else{
+  #   V = array(cor(hatB[[i]][idx, ]), dim = c(C, C, G))
+  #   V[is.na(V)] = 0
+  # }
+  # for (kk in 1:C){
+  #   V[kk, kk, ] = 1
+  # }
+  
+  # Initialization
+  M <- nrow(R)
+  init = Initialize_pi_U(hatB, hatS, C, M)
+  
+  if (is.null(pi.init)){
+    pi.init = init$pi
+    U.orig = init$U
+  }
+  pi = pi.init
+  U = U.orig
+  L = length(pi)
+  
+  
+  ## Train with only marginally significant SNPs
+  M0 = nrow(R)
+  ME_p <- 2 - 2 * pnorm(abs(hatB / hatS), 0, 1)
+  idx <- which(ME_p <= significant_thres, arr.ind = TRUE)[, 1] %>% unique
+  if (length(idx) == 1){
+    idx = c(idx - 1, idx, idx + 1)
+    idx = idx[idx >= 1 & idx <= nrow(hatB)]
+  }
+  hatB = hatB[idx, ]
+  R = R[idx, idx]
+  
+  M1 = length(idx)
+  pi.in = M1 / M0
+  M <- nrow(R)
+  if (M1 == 0){
+    return(list(pi = 1, U = list(matrix(0, C, C)), V = V, n.iter = 0))
+  }
+  
+  
+  # if (!V.fix){
+  #   iRB <- vector("list", G)
+  #   for (j in 1:G){
+  #     iRB[[j]] = t(hatB[[j]]) %*% solve(R[[j]]) %*% hatB[[j]]
+  #   }
+  # }
+  
+  J <- 0
+  g <- list()
+
+  kk = 1
+  patterns.old = names(U)
+  repeated_pattern = 0
+  
+  # MCEM steps
+  for (kk in 1:n.iter){
+    # cat("\n iter:", kk, " ")
+
+    # E-step
+    ## MC step
+    # init
+    nsim = 1
+    gg = matrix(paste(rep(0, C), collapse = ""), M, MC.sim[kk])
+    BB = array(0, dim = c(M, C, MC.sim[kk]))
+    
+    # MC iter
+    gBc = gB_coef(U, V)
+    while (nsim < MC.sim[kk]){
+      BB[, , nsim + 1] = gBupdate(B = BB[, , nsim], hatB = hatB,
+                    R = R, pi = pi, h = h,
+                    TT = gBc$TT, TT_det = gBc$TT_det, mu1 = gBc$mu1, Sigma1 = gBc$Sigma1)
+      
+      nsim  = nsim + 1
+      gg[, nsim] = ifelse(BB[, , nsim] != 0, 1, 0) %>% apply(1, paste, collapse = "")
+    }
+    
+    samp.ind = ceiling(MC.sim[kk] / 3):MC.sim[kk]
+    patterns = unique(c(gg[, samp.ind]))
+    zero_pattern = paste(rep(0, C), collapse = "")
+    if (zero_pattern %in% patterns) {
+      patterns <- c(patterns[patterns != zero_pattern], zero_pattern)
+    } else {
+      patterns <- c(patterns, zero_pattern)
+    }
+    g = apply(gg[, samp.ind], 1, function(x) table(factor(x, levels = patterns))) %>% t 
+    
+    Sigma = vector("list", M)
+    L = length(patterns)
+    
+    if (L <= 1){
+      return(list(pi = pi, U = U, V = V, n.iter = kk, pi.in = pi.in, M1 = M1))
+    }
+    
+    for (k in 1:M){
+      Sigma[[k]] = vector("list", L - 1)
+      for (l in seq(L-1)){
+        ind = which(gg[k, samp.ind] == patterns[l]) + samp.ind[1] - 1
+        if (length(ind) > 0){
+          Sigma[[k]][[l]] = tcrossprod(BB[k, , ind]) / length(ind)
+        } else{
+          Sigma[[k]][[l]] = matrix(0, C, C)
+        }
+      }
+    }
+    
+    # Update V's
+    #   if (!V.fix){
+    #     first_mom = t(hatB[[j]]) %*% apply(BB[[j]][, , samp.ind], 1:2, mean)
+    #     first_mom = first_mom + t(first_mom)
+    #     second_mom = matrix(0, C, C)
+    #     for (sam in samp.ind){
+    #       second_mom = second_mom + t(BB[[j]][, , sam]) %*% R[[j]] %*% BB[[j]][, , sam]
+    #     }
+    #     second_mom = second_mom / length(samp.ind)
+    #     V[, , j] = (iRB[[j]] - first_mom + second_mom) / M[j]
+    #   }
+    # }
+    
+    # End of E-step
+    
+    # g_jkt
+    if (kk <= 2){
+      g = t(apply(g, 1, replace_nonzero_with_mean))
+    }
+    g <- g / sum(g)
+    
+    # terminations:
+    pi.new  = colSums(g) * pi.in
+    pi.new[L] = 1 - sum(pi.new[-L])
+    
+    if (length(pi) == L){
+      if (max(abs(pi.new - pi) / pi) <= tol){
+        break
+      }
+    }
+    
+    # M-step
+    # Update pi's
+    if (!pi.fix){
+      pi = pi.new
+    }
+    
+    # Update U's
+    U = vector("list", L)
+    for (l in 1:(L-1)){
+      U[[l]] <- matrix(0, C, C)
+      for (k in 1:M){
+        U[[l]] = U[[l]] + g[k, l] * Sigma[[k]][[l]]
+      }
+      U[[l]] <- U[[l]] / pi[l] * pi.in
+      U[[l]][abs(U[[l]]) < 1e-5 / M] = 0
+    }
+    U[[L]] <- matrix(0, C, C)
+    names(U) = patterns
+    
+    #########################################
+    # Cut pi's
+    if (kk <= 5){
+      ll = which(pi < 1e-8)
+    } else{
+      ll = which(pi < 1e-2 / M * pi.in)
+    }
+    if (length(ll) > 0){
+      # cat("pi delete", ll, "\n")
+      U = U[-ll]
+      pi = pi[-ll]
+    }
+    
+    if (length(pi) <= 1){
+      return(list(pi = pi, U = U, V = V, n.iter = kk, pi.in = pi.in, M1 = M1))
+    }
+    
+    pi = pi / sum(pi)
+    L = length(U)
+    
+    # Update and cut U's
+    ll = integer(0)
+    dd = 0
+    for (l in 1:(L-1)){
+      ind = which(diag(U[[l]]) <= qchisq(0.95, 1) * diag(V))
+      U[[l]][ind, ] = 0
+      U[[l]][, ind] = 0
+      lp = 0
+      flag = TRUE
+      while (flag & (lp < L)){
+        lp = lp + 1
+        if (mean( (U[[l]] != 0) == (U[[lp]] != 0) ) == 1 & (l != lp) & !(lp %in% ll)){
+          dd = dd + 1
+          ll[dd] = l
+          U[[lp]] = (pi[l] * U[[l]] + pi[lp] * U[[lp]]) / (pi[l] + pi[lp])
+          pi[lp] = pi[l] + pi[lp]
+          flag = FALSE
+        }
+      }
+    }
+    
+    if (length(ll) > 0){
+      # cat("U delete", ll, "\n")
+      U = U[-ll]
+      pi = pi[-ll]
+    }
+    
+    
+    L = length(U)
+    if (L <= 1){
+      return(list(pi = pi, U = U, V = V, n.iter = kk, pi.in = pi.in, M1 = M1))
+    }
+    
+    # cat("\n", names(pi), "\n")
+    # print(pi)
+    if (setequal(patterns.old, names(pi))){
+      repeated_pattern = repeated_pattern + 1
+    } else{
+      repeated_pattern = 0
+      patterns.old = names(pi)
+    }
+    
+    if (repeated_pattern >= 10){
+      return(list(pi = pi, U = U, V = V, n.iter = kk, pi.in = pi.in, M1 = M1))
+    }
+  }
+  
+  return(list(pi = pi, U = U, V = V, n.iter = kk, pi.in = pi.in, M1 = M1))
+}
+
+
+#' CASE Model Testing
+#'
+#' Obtain the posterior probabilities and estimates of the cis-eQTL effect sizes.
+#' @param Z (M * C),The z scores for eQTL summary statistics.
+#' @return The temperature in degrees Celsius
+#' @examples 
+#' temp1 <- F_to_C(50);
+#' temp2 <- F_to_C( c(50, 63, 23) );
+#' @export
+ED_test <- function(hatB = NULL, Z = NULL, R, N, V, U, pi, MC.sim = 41, MC.sample = 58){
+  # Here V is V adjusted for sample sizes
+  #### Testing ####
+  if (is.null(Z)){
+    Z = hatB / hatS
+  }
+  
+  hatBS = transform_Z(Z, N)
+  hatB = hatBS$hatB
+  hatS = hatBS$hatS
+  
+  pi = pi / sum(pi)
+  C <- ncol(hatB)
+  M <- nrow(R)
+  
+  if (length(pi) <= 1){
+    post_mean = matrix(0, M, C)
+    pvalue = matrix(1, M, C)
+    return(list(pi = pi, U = U, V = V, pvalue = pvalue, post_mean = post_mean))
+  }
+  
+  # m_split = U_split(U, pi, V)
+  # U = m_split$U
+  # pi = m_split$pi
+  
+  L = length(U)
+  ## MC step
+  gBc = gB_coef(U, V)
+  pp = pm = array(0, dim = c(M, C, MC.sample))
+
+  for (ll in 1:MC.sample){
+    nsim = 1
+    BB = array(0, dim = c(M, C, MC.sim))
+    while (nsim < MC.sim){
+      gB = gBupdate(B = BB[, , nsim],
+                    hatB = hatB,
+                    R = R, pi = pi,
+                    TT = gBc$TT, TT_det = gBc$TT_det,
+                    mu1 = gBc$mu1, Sigma1 = gBc$Sigma1)
+
+      nsim  = nsim + 1
+      BB[, , nsim] = gB
+    }
+
+    pp[, , ll] = apply(BB[, , -(1:ceiling(MC.sim * 0.3))], 1:2, function(x) mean(x == 0))
+    pm[, , ll] = apply(BB[, , -(1:ceiling(MC.sim * 0.3))], 1:2, mean)
+
+    if (ll == 20 & max(apply(pp[, , 1:ll], 1:2, sd)) <= 0.05){
+      break
+    }
+  }
+  pvalue = apply(pp[, , 1:ll], 1:2, mean)
+  post_mean = apply(pm[, , 1:ll], 1:2, mean)
+
+  return(list(pi = pi, U = U, V = V, pvalue = pvalue, post_mean = post_mean))
+}
+
+
+##########
+
+#' CASE Obtain Credible Sets
+#'
+#' Obtain credible sets for any multi-trait fine-mapping results.
+#' @param pvalues (M * C),The pvalues of SNPs.
+#' @return The temperature in degrees Celsius
+#' @examples 
+#' temp1 <- F_to_C(50);
+#' temp2 <- F_to_C( c(50, 63, 23) );
+#' @export
+get_credible_sets <- function(pvalues, R, cor.min = 0.5, pip = 0.95, ruled_out = 1 - 1e-4){
+  C = ncol(pvalues)
+  css = vector("list", C)
+  R1 = R
+  
+  for (ct in 1:C){
+    p = pvalues[, ct]
+    
+    or = order(p)
+    cs = list()
+    coverage = numeric(0)
+    purity = numeric(0)
+    L = 0
+    flag = rep(TRUE, nrow(R))
+    
+    for (kk in or){
+      if (p[kk] > 0.95){
+        break
+      }
+      
+      if (p[kk] <= 1 - pip){
+        L = L + 1
+        cs[[L]] = kk
+        flag[cs[[L]]] = FALSE
+        purity[L] = 1
+        coverage[L] = 1 - p[kk]
+      } else{
+        inds = which(abs(R[kk, ]) >= cor.min & flag)
+        if (sum(1 - p[inds]) >= pip){
+          or_inds = order(p[inds])
+          or_inds = or_inds[p[inds[or_inds]] <= ruled_out]
+          if (sum(1-p[inds[or_inds]]) > pip){
+            best_local_sets = select_first_valid_set(1 - p[inds[or_inds]], R[inds[or_inds], inds[or_inds]],
+                                                     pip, cor.min)
+            if (!is.null(best_local_sets)){
+              L = L + 1
+              cs[[L]] = inds[or_inds[best_local_sets]]
+              coverage[L] = min(1, sum(1 - p[cs[[L]]]))
+              mat = abs(R[cs[[L]], cs[[L]]])
+              purity[L] = min(mat[upper.tri(mat)])
+              flag[cs[[L]]] = FALSE
+            }
+          }
+        }
+      }
+      flag[kk] = FALSE
+    }
+    
+    css[[ct]] = list(cs = cs, purity_min_cor = purity, coverage = coverage)
+  }
+  return(css)
+}
---
+++ R/CASE_uitility.R
@@ -0,0 +1,306 @@
+transform_Z <- function(Z, N){
+  C = ncol(Z)
+  hatB = hatS = Z
+  if (length(dim(N)) == 2){
+    N = diag(N)
+  }
+  
+  for (c in 1:C){
+    hatB[, c] <- Z[, c] * sqrt((N[c] - 1) / N[c] / (N[c] - 1 + Z[, c]^2))
+    hatS[, c] <- sqrt(1 / N[c] - hatB[, c]^2 / (N[c] - 1))
+  }
+  return(list(hatB = hatB, hatS = hatS))
+}
+
+select_min_length_set <- function(p, R, threshold_sum = 0.95, min_corr = 0.5) {
+  N <- length(p)
+  best_set <- NULL  # To track the best (smallest) valid set
+  best_set_length <- N + 1  # To track the length of the best set (initially set to larger than N)
+  
+  # Helper function to check if adding a new element keeps the correlation valid
+  is_valid_set <- function(selected, new_idx) {
+    if (length(selected) == 0) {
+      return(TRUE)
+    }
+    # Check if all correlations with the current selected set are >= min_corr
+    return(all(abs(R[new_idx, selected]) >= min_corr))
+  }
+  
+  # Recursive backtracking function to explore combinations
+  backtrack <- function(selected, current_sum, idx) {
+    # If the current sum exceeds the threshold, check if this is the smallest valid set
+    if (current_sum >= threshold_sum) {
+      if (length(selected) < best_set_length) {
+        best_set <<- selected
+        best_set_length <<- length(selected)
+      }
+      return()
+    }
+    
+    if (idx <= N) {
+      for (i in idx:N) {
+        if (is_valid_set(selected, i)) {
+          backtrack(c(selected, i), current_sum + p[i], i + 1)
+        }
+      }
+    }
+  }
+  
+  # Start backtracking with the first element already included
+  backtrack(selected = 1, current_sum = p[1], idx = 2)
+  
+  return(best_set)
+}
+
+select_first_valid_set <- function(p, R, threshold_sum = 0.95, min_corr = 0.5) {
+  N <- length(p)
+  
+  # Helper function to check if adding a new element keeps the correlation valid
+  is_valid_set <- function(selected, new_idx) {
+    if (length(selected) == 0) {
+      return(TRUE)
+    }
+    # Check if all correlations with the current selected set are >= min_corr
+    return(all(abs(R[new_idx, selected]) >= min_corr))
+  }
+  
+  # Recursive backtracking function to find the first valid set
+  backtrack <- function(selected, current_sum, idx) {
+    # If the current sum exceeds the threshold, return the selected set
+    if (current_sum >= threshold_sum) {
+      return(selected)
+    }
+    if (current_sum + sum(p[idx:N]) < threshold_sum){
+      return(NULL)
+    }
+    if (idx < N) {
+      if (length(selected) == 1){
+        loops = 1:(N-idx+1)
+      }else{
+        loops = which(apply(abs(R[idx:N, selected]), 1, min) >= min_corr)
+      }
+      loops = loops + idx - 1
+      
+      if (length(loops) > 0){
+        for (i in 1:length(loops)) {
+          if (current_sum + sum(p[loops[i:length(loops)]]) < threshold_sum){
+            return(NULL)
+          }
+          if (i < length(loops)){
+            result <- backtrack(c(selected, loops[i]), current_sum + p[loops[i]], loops[i + 1])
+          } else{
+            if (current_sum + p[loops[i]] >= threshold_sum){
+              return(c(selected, loops[i]))
+            }
+          }
+          
+          if (!is.null(result)) {
+            return(result)  # Return the result as soon as a valid set is found
+          }
+        }
+      }
+    } else{
+      if (all(abs(R[N, selected]) >= min_corr) & current_sum + p[N] >= threshold_sum){
+        return(c(selected, N))
+      }
+    }
+    return(NULL)
+  }
+  
+  # Start backtracking with the first element already included
+  return(backtrack(selected = 1, current_sum = p[1], idx = 2))
+}
+
+replace_nonzero_with_mean <- function(row) {
+  # Find non-zero and non-last elements
+  non_zero_non_last_indices <- which(row != 0 & seq_along(row) != length(row))
+  
+  # Compute the mean of the non-zero and non-last elements
+  if (length(non_zero_non_last_indices) > 0) {
+    mean_value <- mean(row[non_zero_non_last_indices])
+    row[non_zero_non_last_indices] <- mean_value
+  }
+  
+  return(row)
+}
+
+gB_coef <- function(U, V){
+  C = nrow(U[[1]])
+  L = length(U)
+  
+  TT = array(NA, dim = c(C, C, L))
+  TT_det = rep(NA, L)
+  mu1 = array(NA, dim = c(C, C, L))
+  Sigma1 = array(NA, dim = c(C, C, L))
+  
+  for (l in 1:L){
+    TT[, , l] = ginv(V + U[[l]])
+    TT_det[l] = determinant(TT[, , l], logarithm = TRUE)$modulus
+    
+    mu1[, , l] = U[[l]] %*% TT[, , l]
+    Sigma1[, , l] = U[[l]] - U[[l]] %*% TT[, , l] %*% U[[l]]
+    Sigma1[, , l] = (Sigma1[, , l] + t(Sigma1[, , l])) / 2
+  }
+  return(list(TT = TT, TT_det = TT_det, mu1 = mu1, Sigma1 = Sigma1))
+}
+
+gBupdate <- function(B, hatB, R, pi, h = NULL,
+                     TT, TT_det, mu1, Sigma1){
+  C = dim(hatB)[2]
+  M = nrow(B)
+  L = length(pi)
+  
+  # update g by coord
+  res = hatB - R %*% B + B # B + V
+  
+  prob = ((res %*% TT[, , L]) * res)
+  inds1 = which(prob >= qchisq(1 - 5e-3 / 2, 1), arr.ind = TRUE)[, 1] %>% unique
+  prob = rowSums(prob)
+  inds2 = which(prob > max(quantile(prob, 0.9), qchisq(1 - 5e-3 / 2, C)))
+  
+  
+  SNPS = union(inds1, inds2)
+  prob = prob[SNPS] / 3
+  
+  or = integer(0)
+  while (length(prob) > 0){
+    prob.in = exp(prob - max(prob))
+    index.in = which(prob.in >= 0.01)
+    if (length(index.in) == 1){
+      sel = index.in
+    }else{
+      Mc = sum(prob.in >= 0.1)
+      sel = sample(index.in, Mc, replace = FALSE, prob = prob.in[index.in])
+    }
+    
+    or = c(or, SNPS[sel])
+    prob = prob[-sel]
+    SNPS = SNPS[-sel]
+  }
+  
+  for (i in or){
+    res = hatB[i, ] - t(B) %*% R[, i] + B[i, ]
+    
+    pi1 = TT_det / 2 + log(pi)
+    
+    for (l in 1:L){
+      pi1[l] = pi1[l] - (t(res) %*% TT[, , l] %*% res) / 2
+    }
+    
+    pi1 = exp(pi1 - max(pi1))
+    g = rmultinom(1, 1, pi1) %>% apply(2, function(x) which(x == 1))
+    
+    if (g == L){
+      B[i, ] = 0
+    } else{
+      mu = c(mu1[, , g] %*% res)
+      sigma0 = Sigma1[, , g]
+      
+      if (!is.null(h)){
+        fake_thres = rbind(sqrt(h / 40), qnorm(0.95) / sqrt(diag(TT[, , L]))) %>% apply(2, max)
+      } else{
+        fake_thres = qnorm(0.95) / sqrt(diag(TT[, , L]))
+      }
+      fake_idx = which(abs(mu) < fake_thres)
+      mu[fake_idx] = 0
+      sigma0[, fake_idx] = 0
+      sigma0[fake_idx, ] = 0
+      
+      B[i, ] = mvtnorm::rmvnorm(1, mean = mu, sigma = sigma0)
+      if (any(abs(B[i, ]) < 1e-5)){
+        B[i, ][abs(B[i, ]) < 1e-5] = 0
+      }
+    }
+  }
+  
+  B[-or, ] = 0
+  
+  return(B)
+}
+
+# pupdate <- function(B, hatB, R, pi, TT, TT_det, mu1, Sigma1){
+#   C = dim(hatB)[2]
+#   M = nrow(B)
+#   L = length(pi)
+#   
+#   CASE1 = post1 = matrix(0, M, C)
+#   for (i in 1:M){
+#     res = hatB[i, ] - t(B) %*% R[, i]
+#     pi1 = TT_det / 2 + log(pi)
+#     
+#     for (l in 1:length(pi)){
+#       pi1[l] = pi1[l] - (t(res) %*% TT[, , l] %*% res) / 2
+#     }
+#     
+#     pi1 = exp(pi1 - max(pi1))
+#     pi1 = pi1 / sum(pi1)
+#     
+#     for (l in 1:L){
+#       post = mu1[, , l] %*% res
+#       if (any(abs(post) < 1e-8)){
+#         post[abs(post) < 1e-8] = 0
+#       }
+#       CASE1[i, ] = CASE1[i, ] + pi1[l] * (post != 0)
+#       post1[i, ] = post1[i, ] + pi1[l] * post
+#     }
+#   }
+#   
+#   return(list(CASE1 = CASE1, post1 = post1))
+# }
+
+canonical_patterns <- function(C) {
+  single_bit_strings <- sapply(1:C, function(i) {
+    binary_string <- rep(0, C)  # Create a vector of C zeroes
+    binary_string[i] <- 1       # Set the ith position to 1
+    paste(binary_string, collapse = "")  # Convert to string
+  })
+  
+  all_one_string <- paste(rep(1, C), collapse = "")
+  
+  result <- c(single_bit_strings, all_one_string)
+  return(result)
+}
+
+adjust_sumstats <- function(hatB, hatS, method = "BH"){
+  ME_p <- 2 - 2 * pnorm(abs(hatB / hatS), 0, 1)
+  ME_p <- apply(ME_p, 2, function(x) p.adjust(x, method = method))
+  return(ME_p)
+}
+
+Initialize_pi_U <- function(hatB, hatS, C, M, sig_threshold = 0.1){
+  ME_p = adjust_sumstats(hatB, hatS)
+  patterns <- ifelse(ME_p <= sig_threshold, 1, 0) %>% apply(1, paste, collapse = "")
+  
+  # Add canonical patterns and move zero pattern to the last.
+  patterns = unique(c(patterns, canonical_patterns(C)))
+  zero_pattern = paste(rep(0, C), collapse = "")
+  if (zero_pattern %in% patterns) {
+    patterns <- c(patterns[patterns != zero_pattern], zero_pattern)
+  } else {
+    patterns <- c(patterns, zero_pattern)
+  }
+  
+  cat("Initialize with patterns: ", patterns, "\n")
+  
+  L = length(patterns)
+  if (L > 6){
+    pi = c(rep(5 / M / (L - 1), L - 1), (M - 5) / M)
+  }
+  
+  
+  U = list()
+  for (l in patterns){
+    U[[l]] = matrix(.1, C, C)
+    diag(U[[l]]) = 1
+    idx = which(as.numeric(strsplit(l, "")[[1]]) == 0)
+    U[[l]][idx, ] = 0
+    U[[l]][, idx] = 0
+  }
+  
+  DD = apply(abs(hatB), 2, max)
+  U = lapply(U, function(x) (x * DD) %*% diag(DD))
+  
+  return(list(pi = pi, U = U))
+}
+
+###################
